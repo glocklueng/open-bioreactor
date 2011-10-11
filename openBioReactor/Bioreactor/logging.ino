60d5b7ae2794a45bd138d5286025eb9102d4c15a
lpatiny <lpatiny@a9341e05-f52e-d854-dc2e-c3bc20106509>
2011-10-11 07:11:41 +0000

    Put the comma on one line, there is still a bug in String concatenation otherwise

--- openBioReactor/Bioreactor/logging.ino
+++ openBioReactor/Bioreactor/logging.ino
@@ -24,50 +24,32 @@ void loggingEvent()
   char buffer[5];
    //Log all variable data from all sensors 
    //print out the following: TimeStamp[sec],ReactorID[ID],TempSet[C],TempInLiquid[C],TempBottomPlate[C],TempAmbient[C],HeatingPIDregOutput[0;5000],
-   logDataString = String(ethernetGetTimeStamp());
-   logDataString += ",";
-   logDataString += String(BIOREACTOR_ID);
-   logDataString += ",";
+   logDataString = String(ethernetGetTimeStamp())+",";
+   logDataString += String(BIOREACTOR_ID)+",";
    globalFloatToStringDouble(HEATING_TEMPERATURE_LIMIT,2,buffer);
-   logDataString += String(buffer); //TempSet
-   logDataString += ",";
+   logDataString += String(buffer)+","; //TempSet
    globalFloatToStringDouble(temperatureMeasuredInLiquid(),2,buffer);
-   logDataString += String(buffer); //TempInLiquid
-   logDataString += ",";
+   logDataString += String(buffer)+","; //TempInLiquid
    globalFloatToStringDouble(temperatureMeasuredBottomPlate(),2,buffer);
-   logDataString += String(buffer); //TempBottomPlate
-   logDataString += ",";
+   logDataString += String(buffer)+","; //TempBottomPlate
    globalFloatToStringDouble(temperatureMeasuredAmbient(),2,buffer);
-   logDataString += String(buffer); //TempAmbient
-   logDataString += ",";   
-   logDataString += String(heatingPIDRegOutput());
-   logDataString += ",";
+   logDataString += String(buffer)+","; //TempAmbient
+   logDataString += String(heatingPIDRegOutput())+",";
    // print out the following LiquidLevel[inch],LiquidIN[secON/60sec],LiquidOUT[secON/60sec],pH[1;14],AcidIN[secON/60sec],BaseIn[secON/60sec],
    globalFloatToStringDouble(liquidLevelGet(),2,buffer);
-   logDataString += String(buffer); //Liquid level hight //for debugging in inch
-   logDataString += ",";
-   logDataString += -1; //LiquidIN[secON/60sec]
-   logDataString += ",";
-   logDataString += -1; //LiquidOUT[secON/60sec]
-   logDataString += ",";
+   logDataString += String(buffer)+","; //Liquid level hight //for debugging in inch
+   logDataString += String(-1)+","; //LiquidIN[secON/60sec]
+   logDataString += String(-1)+","; //LiquidOUT[secON/60sec]
    globalFloatToStringDouble(pHmeterGet(),2,buffer);
-   logDataString += String(buffer); //pH level
-   logDataString += ",";
-   logDataString += String(-1); //AcidIN[secON/60sec]
-   logDataString += ",";   
-   logDataString += String(-1); // BaseIn[secON/60sec]
-   logDataString += ",";
-   logDataString += String(relaySwitchMotorGetState());
-   logDataString += ",";
+   logDataString += String(buffer)+","; //pH level
+   logDataString += String(-1)+","; //AcidIN[secON/60sec]
+   logDataString += String(-1)+","; // BaseIn[secON/60sec]
+   logDataString += String(relaySwitchMotorGetState())+",";
     // print out the following MotorState[OFF;ON],NH4[concentr],GazCH4IN[secON/60sec],GazCO2IN[secON/60sec],BioreactorState[0:2]
-   logDataString += String(-1); //NH4[concentr]
-   logDataString += ",";   
-   logDataString += String(-1); // GazCH4IN[secON/60sec]
-   logDataString += ",";
-   logDataString += String(-1); //GazCO2IN[secON/60sec]
-   logDataString += ",";
-   logDataString += String(-1);  //GazN2IN[secON/60sec]
-   logDataString += ",";
+   logDataString += String(-1)+","; //NH4[concentr]
+   logDataString += String(-1)+","; // GazCH4IN[secON/60sec]
+   logDataString += String(-1)+","; //GazCO2IN[secON/60sec]
+   logDataString += String(-1)+",";  //GazN2IN[secON/60sec]
    logDataString += String(BIOREACTOR_MODE);
    //no line end! Threat this separately for logging into the SD card and via ethernet on the server
    
