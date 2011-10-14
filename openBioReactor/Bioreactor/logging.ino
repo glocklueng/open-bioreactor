64bc81974284d08c5d4dc9c81bab1c5a9413beca
lpatiny <lpatiny@a9341e05-f52e-d854-dc2e-c3bc20106509>
2011-10-14 05:38:02 +0000

    Circumvent the sprintf float problem

--- openBioReactor/Bioreactor/logging.ino
+++ openBioReactor/Bioreactor/logging.ino
@@ -5,7 +5,7 @@ char logDataString[LOG_MAX_LENGTH];
 
 void loggingSetup()
 {
-   Serial.println("Logging successfully initialized.");
+  Serial.println("Logging successfully initialized.");
 }
 
 void loggingEvent()
@@ -15,31 +15,53 @@ void loggingEvent()
 
   if(DEBUG)Serial.println("logging...");
 
-   //Log all variable data from all sensors 
-   //print out the following: TimeStamp[sec],ReactorID[ID],TempSet[C],TempInLiquid[C],TempBottomPlate[C],TempAmbient[C],HeatingPIDregOutput[0;5000],
-   //                         LiquidLevel[inch],LiquidIN[secON/60sec],LiquidOUT[secON/60sec],pH[1;14],AcidIN[secON/60sec],BaseIn[secON/60sec],
-   //                         MotorState[OFF;ON],NH4[concentr],GazCH4IN[secON/60sec],GazCO2IN[secON/60sec],BioreactorState[0:2]
+  /*
+  Log all variable data from all sensors 
+   print out the following: TimeStamp[sec],ReactorID[ID],TempSet[C],TempInLiquid[C],TempBottomPlate[C],TempAmbient[C],
+   HeatingPIDregOutut[0; 5000],LiquidLevel[inch],LiquidIN[secON/60sec],LiquidOUT[secON/60sec],pH[1;14],AcidIN[secON/60sec],
+   BaseIn[secON/60sec],MotorState[OFF;ON],NH4[concentr],GazCH4IN[secON/60sec],GazCO2IN[secON/60sec],BioreactorState[0:2]
+   */
+
+
 
   logDataStringLength = sprintf(logDataString, 
-          "%lu,%d,%.2f,%.2f,%.2f,%.2f,%.2f,%.2f,%d,%d,%.2f,%d,%d,%d,%d,%d,%d,%d,%d",
-          ethernetGetTimeStamp(),BIOREACTOR_ID,HEATING_TEMPERATURE_LIMIT,temperatureMeasuredInLiquid(),
-          temperatureMeasuredBottomPlate(),temperatureMeasuredAmbient(),heatingPIDRegOutput(),
-          liquidLevelGet(),notImplemented,notImplemented,pHmeterGet(),notImplemented,notImplemented,relaySwitchMotorGetState(),
-          notImplemented,notImplemented,notImplemented,notImplemented,BIOREACTOR_MODE);
+  "%lu,%d,%0d.%d,%0d.%d,%.2f,%.2f,%.2f,%.2f,%d,%d,%.2f,%d,%d,%d,%d,%d,%d,%d,%d",
+  ethernetGetTimeStamp(),
+  BIOREACTOR_ID,
+  HEATING_TEMPERATURE_LIMIT, (int)((  HEATING_TEMPERATURE_LIMIT - (int)  HEATING_TEMPERATURE_LIMIT)*100),
+  temperatureMeasuredInLiquid(), (int)((temperatureMeasuredInLiquid() - (int)temperatureMeasuredInLiquid())*100),
+  temperatureMeasuredBottomPlate(),
+  temperatureMeasuredAmbient(),
+  heatingPIDRegOutput(),
+  liquidLevelGet(),
+  notImplemented,
+  notImplemented,
+  pHmeterGet(),
+  notImplemented,
+  notImplemented,
+  relaySwitchMotorGetState(),
+  notImplemented,
+  notImplemented,
+  notImplemented,
+  notImplemented,
+  BIOREACTOR_MODE
+    );
 
-   //no line end! Threat this separately for logging into the SD card and via ethernet on the server
-   
-   
-   if(DEBUG)
-   {
-     Serial.println(logDataString);
-     Serial.print("Size of the string is: ");
-     Serial.println(logDataStringLength);
-   }
+  //no line end! Threat this separately for logging into the SD card and via ethernet on the server
 
-   ethernetPushLog(logDataString); 
+
+  if(DEBUG)
+  {
+    Serial.println(logDataString);
+    Serial.print("Size of the string is: ");
+    Serial.println(logDataStringLength);
+  }
+
+  ethernetPushLog(logDataString); 
 
 }
+
+
 
 
 
