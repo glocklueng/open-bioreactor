c2b361d761cb3fb61bc667b0458a79ede5ed7fdd
oliver.peric@gmail.com <oliver.peric@gmail.com@a9341e05-f52e-d854-dc2e-c3bc20106509>
2011-11-16 03:39:21 +0000

    First implementation for auto-switching for gas valves. Only one operational. Fixing of bugs with gas valve's parameter logging.

--- openBioReactor/Bioreactor/gasValves.ino
+++ openBioReactor/Bioreactor/gasValves.ino
@@ -5,14 +5,19 @@ int gasValvesGetState(int gasValveID);
 int gasValvesGetAutoSwitchInterval(int gasValveID);
 void gasValvesSetAutoSwitchInterval(int gasValveID, int percent);
 void gasValvesAutoSwitchMode();
-
+void gasValvesCheck();
 
 struct gasValveStruct { int state;
                         int autoSwitchInterval; // in [%] percent [0:100]
+                        boolean autoSwitchFlag;
+                        int autoSwitchState;
                         int arduinoPin;  
+                        unsigned long timestampOn;
+                        unsigned long timestampOff;
                        };
 
 struct gasValveStruct gasValve[3];
+
 
 void gasValvesSetup()
 {
@@ -22,12 +27,15 @@ void gasValvesSetup()
    
    gasValve[CH4].state = OFF;
    gasValve[CH4].autoSwitchInterval = 0; // percent
+   gasValve[CH4].autoSwitchFlag = false;
    gasValve[CH4].arduinoPin = PIN_GAS_CH4;
    gasValve[CO2].state = OFF;
    gasValve[CO2].autoSwitchInterval = 0;
+   gasValve[CO2].autoSwitchFlag = false;
    gasValve[CO2].arduinoPin = PIN_GAS_CO2;
    gasValve[N2].state = OFF;
    gasValve[N2].autoSwitchInterval = 0;
+   gasValve[N2].autoSwitchFlag = false;
    gasValve[N2].arduinoPin = PIN_GAS_N2;
    
 }
@@ -63,7 +71,7 @@ void gasValvesTurnOff(int gasValveID)
        break;
      
      default:
-       Serial.println("ERROR: Invalid Gas valve ID! Gas valve cannot be turned OFF");
+       Serial.println("ERROR: Invalid gas valve ID! Gas valve cannot be turned OFF");
      
    }    
   
@@ -99,7 +107,7 @@ void gasValvesTurnOn(int gasValveID)
        break;
      
      default:
-       Serial.println("ERROR: Invalid Gas valve ID! Gas valve cannot be turned ON");
+       Serial.println("ERROR: Invalid gas valve ID! Gas valve cannot be turned ON");
      
    }    
   
@@ -108,13 +116,15 @@ void gasValvesTurnOn(int gasValveID)
 
 int gasValvesGetState(int gasValveID)
 {
-  if (gasValveID == (CH4 || CO2 || N2))
+  if (gasValveID == CH4
+      || gasValveID == CO2
+      || gasValveID == N2)
   {
     return gasValve[gasValveID].state;
   }
   else
   {
-    Serial.println("ERROR: Invalid Gas valve ID! Gas valve's state cannot be returned.");
+    Serial.println("ERROR: Invalid gas valve ID! Gas valve's state cannot be returned.");
     return -1;   
   }    
 }
@@ -122,13 +132,15 @@ int gasValvesGetState(int gasValveID)
 
 int gasValvesGetAutoSwitchInterval(int gasValveID) // GET
 {
-  if (gasValveID == (CH4 || CO2 || N2))
+  if (gasValveID == CH4
+      || gasValveID == CO2
+      || gasValveID == N2)
   {
     return gasValve[gasValveID].autoSwitchInterval;
   }
   else
   {
-    Serial.println("ERROR: Invalid Gas valve ID!Gas valve's AutoSwitchInterval cannot be returned.");
+    Serial.println("ERROR: Invalid gas valve ID! Gas valve's AutoSwitchInterval cannot be returned.");
     return -1;   
   }    
 }
@@ -138,15 +150,120 @@ int gasValvesGetAutoSwitchInterval(int gasValveID) // GET
 
 void gasValvesSetAutoSwitchInterval(int gasValveID, int percent) // SET
 {
-    if (gasValveID == (CH4 || CO2 || N2))
+    if (gasValveID == CH4
+        || gasValveID == CO2
+        || gasValveID == N2)    
     {
       gasValve[gasValveID].autoSwitchInterval = percent;
     }
-    else Serial.println("ERROR: Invalid Gas valve ID! Gas valve's AutoSwitchInterval cannot be saved.");
+    else
+    {
+      Serial.println("ERROR: Invalid gas valve ID! Gas valve's AutoSwitchInterval cannot be saved.");
+    }
 }
 
-void gasValvesAutoSwitchMode()
+void gasValvesAutoSwitchMode(int gasValveID)
 {
-    if(DEBUG)Serial.println("Gas valves auto-switching mode is being called successfully.");
+    //preliminary checking of the extreme values
+    if(gasValve[gasValveID].autoSwitchInterval <= 0)
+    {
+       gasValve[gasValveID].state = OFF;
+       gasValve[gasValveID].autoSwitchInterval = 0; //security: if an invalid number has been saved over webUI
+       gasValve[gasValveID].autoSwitchFlag = false;
+       return;
+    }
+    if(gasValve[gasValveID].autoSwitchInterval >= 100)
+    {
+       gasValve[gasValveID].state = ON;
+       gasValve[gasValveID].autoSwitchInterval = 100; //security: if an invalid number has been saved over webUI
+       gasValve[gasValveID].autoSwitchFlag = false;
+       return;
+    }
+
+    
+    //check if gas valve is turned on over WebUI
+    //if so, put the gas valve in auto-switching mode and start the timing
+    if(gasValve[gasValveID].state == ON)
+    {
+      gasValve[gasValveID].autoSwitchFlag = true;
+      gasValve[gasValveID].autoSwitchState = ON;
+      gasValve[gasValveID].timestampOn = millis();
+      gasValve[gasValveID].timestampOff = millis();
+    }
+    else
+    {
+      gasValve[gasValveID].autoSwitchFlag = false; //turn off flag if OFF over webUI
+    }
+    
+    if(gasValve[gasValveID].state == ON && gasValve[gasValveID].autoSwitchFlag == true)
+    {
+      gasValvesCheck();
+      if(DEBUG)Serial.println("Gas valves auto-switching mode is being called successfully."); 
+    }
+    else
+    {
+      Serial.println("WARNING: The gas valve is turned off in the WebUI. Please turn on for auto-swithing mode <100% of gas flow");
+    }
+   
+}
+
+
+// this function is called as well by the main Bioreactor loop
+void gasValvesCheck()
+{
+  unsigned long nowCheck;
+  unsigned long offDuration;
+  unsigned long onDuration;
+  unsigned long timeInterval = 60000; // equals 1 min
+  
+  /*Note: as defined in main: 
+  #define CH4 0
+  #define CO2 1
+  #define N2 2
+  */
+  
+  for(int i=0; i=2; i++)
+  {
+    if(gasValve[i].autoSwitchFlag == true)
+    {
+      onDuration = gasValve[i].autoSwitchInterval * timeInterval;
+      offDuration = timeInterval - onDuration;
+      nowCheck = millis();
+      
+      if((gasValve[i].autoSwitchState == ON) && ((nowCheck - gasValve[i].timestampOn) >= onDuration))
+      {
+        digitalWrite(gasValve[i].arduinoPin, LOW); //switch off
+        gasValve[i].autoSwitchState = OFF;
+        if(DEBUG)
+          {
+            Serial.print("The overtime for valve with ID: ");
+            Serial.print(i);
+            Serial.print(" being ON is [millisec]: ");
+            unsigned long timeDiff = gasValve[i].timestampOn - nowCheck;
+            Serial.println(timeDiff);
+          }
+          
+        gasValve[i].timestampOff = nowCheck; // NOTE: timestampOFF!
+      }//end of onDuration
+            
+      if((gasValve[i].autoSwitchState == OFF) && ((nowCheck - gasValve[i].timestampOff) >= offDuration))
+      {
+        digitalWrite(gasValve[i].arduinoPin, HIGH); //switch on
+        gasValve[i].autoSwitchState = ON;
+        if(DEBUG)
+          {
+            Serial.print("The overtime for valve with ID: ");
+            Serial.print(i);
+            Serial.print(" being OFF is [millisec]: ");
+            unsigned long timeDiff = gasValve[i].timestampOff - nowCheck;
+            Serial.println(timeDiff);
+          }
+          
+        gasValve[i].timestampOn = nowCheck; // NOTE: timestampON!
+      }//end of oFFDuration
+    }
+  }
+
+  
 }
 
